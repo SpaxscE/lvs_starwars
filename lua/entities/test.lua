455c2653b000ed0c1bfb98fdddd61d00120c3e79
Luna <swagnemite@gmx.de>
2023-02-04 22:10:27 +0100

    backup progress

--- lua/entities/test.lua
+++ lua/entities/test.lua
@@ -22,7 +22,7 @@ if SERVER then
 	end
 
 	function ENT:Initialize()	
-		self:SetModel( "models/blu/hsd.mdl" )
+		self:SetModel( "models/blu/hsd_leg_1.mdl" )
 		self:PhysicsInit( SOLID_VPHYSICS )
 		self:SetMoveType( MOVETYPE_VPHYSICS )
 		self:SetSolid( SOLID_VPHYSICS )
@@ -38,34 +38,52 @@ else
 	local Length1 = 140
 	local Length2 = 300
 
-	local LegData = {
+	local Length3 = 20
+	local Length4 = 20
+
+	local LegData1 = {
 		Leg1 = {MDL = "models/blu/hsd_leg_2.mdl", Ang = Angle(0,-90,-90), Pos = Vector(0,0,0)},
 		Leg2 = {MDL = "models/blu/hsd_leg_4.mdl", Ang = Angle(180,90,4), Pos = Vector(20,0,-12)},
 		Foot = {MDL = "models/blu/hsd_foot.mdl", Ang = Angle(0,0,0), Pos = Vector(0,-2,0)}
 	}
 
-	function ENT:Think()
+	local LegData2 = {
+		Leg1 = {MDL = "models/blu/hsd_leg_3.mdl", Ang = Angle(0,90,-90), Pos = Vector(0,0,0)},
+	}
 
-		local ID = self:LookupAttachment( "leg_front_right" )
+	function ENT:Think()
+		--self:SetAngles( Angle(0,math.cos( CurTime() * 2) * 10,0) )
+
+		local ID = self:LookupAttachment( "lower" )
 		local Att = self:GetAttachment( ID )
 
-		local STARTPOS = Att.Pos
-		local ENDPOS = util.TraceLine( { start = STARTPOS, endpos = self:LocalToWorld( Vector(80,-270,-100) ), filter = self } ).HitPos + Vector(0,0,25)
+		local ENDPOS = util.TraceLine( { start = Att.Pos, endpos = self:LocalToWorld( Vector(0,-270,-300) ), filter = self } ).HitPos + Vector(0,0,25)
 
 		local Pos, Ang = WorldToLocal( ENDPOS, (ENDPOS - Att.Pos):Angle(), Att.Pos, self:LocalToWorldAngles( Angle(0,-90,0) ) )
 
-		local JointAngle = self:LocalToWorldAngles( Angle(0,180 + Ang.y,90) )
+		local STARTPOS = Att.Pos
 
-		self:GetLegEnts( 1, Length1, Length2, JointAngle, STARTPOS, ENDPOS, LegData )
+		local JointAngle = self:LocalToWorldAngles( Angle(0,180,90) )
 
-		if self.IK_Joints[ 1 ] then
-			if IsValid( self.IK_Joints[ 1 ].LegBaseRot ) then
-				self.IK_Joints[ 1 ].LegBaseRot:SetAngles( JointAngle )
-			end
-			if IsValid( self.IK_Joints[ 1 ].Attachment3 ) then
-				self.IK_Joints[ 1 ].Attachment3:SetAngles( self:LocalToWorldAngles( Angle(0,Ang.y,0) ) )
-			end
-		end
+		self:GetLegEnts( 1, Length1, Length2, JointAngle, STARTPOS, ENDPOS, LegData1 )
+
+		if not self.IK_Joints[ 1 ] or not IsValid( self.IK_Joints[ 1 ].Attachment2 ) then return end
+
+		local shaft = self.IK_Joints[ 1 ].Attachment2
+
+		local ID1 = self:LookupAttachment( "upper" )
+		local Start = self:GetAttachment( ID1 )
+
+		local ID2 = shaft:LookupAttachment( "upper_end" )
+		local End = shaft:GetAttachment( ID2 )
+
+		self:GetLegEnts( 2, Length3, Length4, JointAngle, Start.Pos, End.Pos, LegData2 )
+
+		if not self.IK_Joints[ 2 ] or not IsValid( self.IK_Joints[ 2 ].Attachment1 ) then return end
+
+		local strut = self.IK_Joints[ 2 ].Attachment1
+		strut:SetPoseParameter( "extrude", (Start.Pos - End.Pos):Length() )
+		strut:InvalidateBoneCache()
 	end
 
 	function ENT:OnRemove()

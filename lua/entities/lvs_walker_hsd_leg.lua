ab3812fe5ed19269263bb7fb8994569b24b12c97
SpaxscE <stephanklings@freenet.de>
2025-11-30 12:59:56 +0100

    make HSD editable

--- lua/entities/lvs_walker_hsd_leg.lua
+++ lua/entities/lvs_walker_hsd_leg.lua
@@ -20,30 +20,30 @@ if SERVER then
 else 
 	include( "entities/lvs_walker_atte/cl_ikfunctions.lua" )
 
-	local Length1 = 140
-	local Length2 = 300
+	ENT.Length1 = 140
+	ENT.Length2 = 300
 
-	local Length3 = 20
-	local Length4 = 20
+	ENT.Length3 = 20
+	ENT.Length4 = 20
 
-	local LegData1 = {
+	ENT.LegData1 = {
 		Leg1 = {MDL = "models/blu/hsd_leg_2.mdl", Ang = Angle(0,-90,-90), Pos = Vector(0,0,0)},
 		Leg2 = {MDL = "models/blu/hsd_leg_4.mdl", Ang = Angle(180,90,4), Pos = Vector(20,0,-12)},
 		Foot = {MDL = "models/blu/hsd_foot.mdl", Ang = Angle(0,0,0), Pos = Vector(0,-2,0)}
 	}
 
-	local LegData2 = {
+	ENT.LegData2 = {
 		Leg1 = {MDL = "models/blu/hsd_leg_3.mdl", Ang = Angle(0,90,-90), Pos = Vector(0,0,0)},
 	}
 
-	local StartPositions = {
+	ENT.StartPositions = {
 		["FL"] = Vector(150,270,0),
 		["FR"] = Vector(150,-270,0),
 		["RL"] = Vector(-150,270,0),
 		["RR"] = Vector(-150,-270,0),
 	}
 
-	local LocToID = {
+	ENT.LocToID = {
 		[1] = "RL",
 		[2] = "FL",
 		[3] = "RR",
@@ -61,14 +61,15 @@ else
 			return
 		end
 
+		local EntTable = self:GetTable()
 		local LocIndex = self:GetLocationIndex()
 
 		if not Base:HitGround() then
-			local Pos = Base:LocalToWorld( StartPositions[ LocIndex ] )
+			local Pos = Base:LocalToWorld( EntTable.StartPositions[ LocIndex ] )
 
 			self:RunIK( Pos, Base )
-			self._OldPos = Pos
-			self._smPos = Pos
+			EntTable._OldPos = Pos
+			EntTable._smPos = Pos
 
 			return
 		end
@@ -81,7 +82,7 @@ else
 		local VelForwardMul = math.min( Speed / 100, 1 )
 		local VelForward = Vel:GetNormalized() * VelForwardMul + Forward * (1 - VelForwardMul)
 
-		local TraceStart = Base:LocalToWorld( StartPositions[ LocIndex ] ) + VelForward * math.Clamp( 400 - Speed * 2, 100, 200 ) * VelForwardMul
+		local TraceStart = Base:LocalToWorld( EntTable.StartPositions[ LocIndex ] ) + VelForward * math.Clamp( 400 - Speed * 2, 100, 200 ) * VelForwardMul
 
 		local trace = util.TraceLine( { 
 			start = TraceStart + Vector(0,0,200),
@@ -93,31 +94,31 @@ else
 			end,
 		} )
 
-		local UpdateLeg = LocToID[ Base:GetUpdateLeg() ] == LocIndex
+		local UpdateLeg = EntTable.LocToID[ Base:GetUpdateLeg() ] == LocIndex
 
-		self._OldPos = self._OldPos or trace.HitPos
-		self._smPos = self._smPos or self._OldPos
+		EntTable._OldPos = EntTable._OldPos or trace.HitPos
+		EntTable._smPos = EntTable._smPos or EntTable._OldPos
 
-		if self._OldUpdateLeg ~= UpdateLeg then
-			self._OldUpdateLeg = UpdateLeg
+		if EntTable._OldUpdateLeg ~= UpdateLeg then
+			EntTable._OldUpdateLeg = UpdateLeg
 
 			if UpdateLeg then
-				self.UpdateNow = true
+				EntTable.UpdateNow = true
 			end
 		end
 
-		if self.UpdateNow and not self.MoveLeg then
+		if EntTable.UpdateNow and not EntTable.MoveLeg then
 			sound.Play( Sound( "lvs/vehicles/hsd/hydraulic_stop0"..math.random(1,2)..".wav" ), self:GetPos(), SNDLVL_100dB )
 
-			self.UpdateNow = nil
-			self.MoveLeg = true
-			self.MoveDelta = 0
+			EntTable.UpdateNow = nil
+			EntTable.MoveLeg = true
+			EntTable.MoveDelta = 0
 		end
 
 		local ShaftOffset = 0
-		local ENDPOS = self._smPos + Up * 20
+		local ENDPOS = EntTable._smPos + Up * 20
 
-		if self.MoveLeg then
+		if EntTable.MoveLeg then
 			local traceWater = util.TraceLine( {
 				start = TraceStart + Vector(0,0,200),
 				endpos = ENDPOS,
@@ -128,8 +129,8 @@ else
 			if traceWater.Hit then
 				local T = CurTime()
 
-				if (self._NextFX or 0) < T then
-					self._NextFX = T + 0.05
+				if (EntTable._NextFX or 0) < T then
+					EntTable._NextFX = T + 0.05
 	
 					local effectdata = EffectData()
 						effectdata:SetOrigin( traceWater.HitPos )
@@ -139,9 +140,9 @@ else
 				end
 			end
 
-			if self.MoveDelta >= 1 then
-				self.MoveLeg = false
-				self.MoveDelta = nil
+			if EntTable.MoveDelta >= 1 then
+				EntTable.MoveLeg = false
+				EntTable.MoveDelta = nil
 
 				sound.Play( Sound( "lvs/vehicles/hsd/footstep0"..math.random(1,3)..".wav" ), ENDPOS, SNDLVL_100dB )
 
@@ -151,17 +152,17 @@ else
 
 				sound.Play( Sound( "lvs/vehicles/hsd/hydraulic_start0"..math.random(1,2)..".wav" ), self:GetPos(), SNDLVL_100dB )
 			else
-				self.MoveDelta = math.min( self.MoveDelta + RealFrameTime() * 2, 1 )
+				EntTable.MoveDelta = math.min( EntTable.MoveDelta + RealFrameTime() * 2, 1 )
 	
-				self._smPos = LerpVector( self.MoveDelta, self._OldPos, trace.HitPos )
+				EntTable._smPos = LerpVector( EntTable.MoveDelta, EntTable._OldPos, trace.HitPos )
 
-				local MulZ =  math.max( math.sin( self.MoveDelta * math.pi ), 0 )
+				local MulZ =  math.max( math.sin( EntTable.MoveDelta * math.pi ), 0 )
 
 				ShaftOffset = MulZ ^ 2 * 30
 				ENDPOS = ENDPOS + Up * MulZ * 50
 			end
 		else
-			self._OldPos = self._smPos
+			EntTable._OldPos = EntTable._smPos
 		end
 
 		self:RunIK( ENDPOS, Base, ShaftOffset )
@@ -169,6 +170,8 @@ else
 
 	function ENT:RunIK( ENDPOS, Base, shaftoffset )
 		shaftoffset = shaftoffset or 0
+
+		local EntTable = self:GetTable()
 
 		local Ang = Base:WorldToLocalAngles( (ENDPOS - self:GetPos()):Angle() )
 
@@ -183,11 +186,11 @@ else
 
 		local STARTPOS = Att.Pos
 
-		self:GetLegEnts( 1, Length1, Length2, self:LocalToWorldAngles( Angle(0,180,135) ), STARTPOS, ENDPOS, LegData1 )
+		self:GetLegEnts( 1, EntTable.Length1, EntTable.Length2, self:LocalToWorldAngles( Angle(0,180,135) ), STARTPOS, ENDPOS, EntTable.LegData1 )
 
-		if not self.IK_Joints[ 1 ] or not IsValid( self.IK_Joints[ 1 ].Attachment2 ) then return end
+		if not EntTable.IK_Joints[ 1 ] or not IsValid( EntTable.IK_Joints[ 1 ].Attachment2 ) then return end
 
-		local shaft = self.IK_Joints[ 1 ].Attachment2
+		local shaft = EntTable.IK_Joints[ 1 ].Attachment2
 
 		shaft:SetPoseParameter( "extrude", shaftoffset )
 		shaft:InvalidateBoneCache()
@@ -202,11 +205,11 @@ else
 
 		if not End then return end
 
-		self:GetLegEnts( 2, Length3, Length4, self:LocalToWorldAngles( Angle(0,0,-45) ), Start.Pos, End.Pos, LegData2 )
+		self:GetLegEnts( 2, EntTable.Length3, EntTable.Length4, self:LocalToWorldAngles( Angle(0,0,-45) ), Start.Pos, End.Pos, EntTable.LegData2 )
 
-		if not self.IK_Joints[ 2 ] or not IsValid( self.IK_Joints[ 2 ].Attachment1 ) then return end
+		if not EntTable.IK_Joints[ 2 ] or not IsValid( EntTable.IK_Joints[ 2 ].Attachment1 ) then return end
 
-		local strut = self.IK_Joints[ 2 ].Attachment1
+		local strut = EntTable.IK_Joints[ 2 ].Attachment1
 		strut:SetPoseParameter( "extrude", (Start.Pos - End.Pos):Length() )
 		strut:InvalidateBoneCache()
 	end
